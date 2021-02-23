69b160f28593718bb3ec2671b6f3e8358807419e
169z87mn <169z87mn@gmail.com>
2021-02-23 23:57:16 +0900

    Proxy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      "/stg/": {
+        target: process.env.VUE_APP_API_GATEWAY_URL
+      }
+    }
+  }
 }
